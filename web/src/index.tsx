d31d7c6720408dacb49bc7bfff402427440e779a
TheGambit <cranenre@gmail.com>
2025-08-04 20:35:22 +0200

    Update August 4 2025, 20:35PM

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { TownScene } from './components/TownScene';
 import { StateManager } from './services/StateManager';
 import { Model } from './services/Model';
-import { CityMap } from './services/CityMap';
 
 const Main: React.FC = () => {
   StateManager.pullParams();
   StateManager.pushParams();
-
-  // This would be part of the component's style
-  // document.body.style.backgroundColor = `#${CityMap.palette.paper.toString(16)}`;
-
-  // Font setup would be handled with CSS
 
   new Model(StateManager.size, StateManager.seed);
 
